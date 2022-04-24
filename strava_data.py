08f3a7faf924be7cbe8973602fe72f540d93a763
matthewestopinal <53093042+matthewestopinal@users.noreply.github.com>
2022-04-24 16:27:01 -0500

    Updated strava_data.py
    Implemented Privacy Zones

--- strava_data.py
+++ strava_data.py
@@ -6,6 +6,11 @@ import gzip
 import shutil
 import os
 
+from geographiclib.geodesic import Geodesic
+import googlemaps
+import json
+
+
 #Accepts the filename of the activity.csv file
 #Returns a list of the filenames for GPS Data
 #TODO Edit to change to a tuple to save some important data (i.e. date)
@@ -50,7 +55,7 @@ def unzip_gps_files(gps_locs, dest="fit_files"):
 
 #Gets the start locations from .fit files in a given folder
 #Given a privacy zone will consider first ping outside privacy zone
-def get_start_locations(src, privacy_zones={}):
+def get_start_locations(src, privacy_zones=None):
     count = 0
     start_locations = []
     for file in os.listdir(src):
@@ -65,13 +70,16 @@ def get_start_locations(src, privacy_zones={}):
 
         start_found = False
 
+        record_debug = []
         for record in fitfile.get_messages("record"):
             #Initialize our location as None
             latitude = None
             longitude = None
             
+            record_debug = []
             #Check all the values in the record
             for data in record:
+                record_debug.append(f"Name: {data.name} Value: {data.value}")
                 if data.name == "position_lat":
                     latitude = data.value
                 if data.name == "position_long":
@@ -86,18 +94,70 @@ def get_start_locations(src, privacy_zones={}):
                     start_found = True
                     break
             
-        #Should be unreached
+        #Note activities with HR data will have fit files without location
         if not start_found:
             print(f"{file} has no valid locations outside of a privacy zone.")
+            #print("Debug log:")
+            #print(record_debug)
 
     print(f"{count} files checked.")
     return start_locations
 
+#Accepts a path to a google maps API key
+#Returns string Key
+def load_gmaps_key(key_loc = 'google_maps_token.txt'):
+    with open(key_loc, 'r') as f:
+        key = f.read()
+        return key
+
 #Checks if the location is within the privacy zone (or in any in the list)
+#Takes loc, tuple of (lat, long) in degrees
+#Takes zones, list of dictionaries
 #Returns boolean, true if it is within a privacy zone
 #TODO Actually implement this
 def check_in_privacy_zone(loc, zones):
+    for zone in zones:
+        #Get radius in kilometers
+        radius = zone["Radius"]
+        radius = radius.split(" ")
+        radius = float(radius[0])
+
+        #GET coords from zone address
+        zone_lat = zone['Latitude']
+        zone_long = zone['Longitude']
+
+        #Get distance in meters
+        geod = Geodesic.WGS84
+        g = geod.Inverse(loc[0], loc[1], zone_lat, zone_long)
+        g_dist = g['s12']
+        if  g_dist < radius * 1000:
+            return True
+
     return False
+
+#Accepts Dataframe of Privacy Zones
+#Returns List of Dictionaries with addres, lat, long, radius
+def get_privacy_coords(zones):
+    if not isinstance(zones, pd.DataFrame):
+        print("Not a DataFrame, returing []")
+        return []
+    
+    privacy_zones = []
+    gmaps_key = load_gmaps_key()
+    gmaps = googlemaps.Client(key=gmaps_key)
+    for index, zone in zones.iterrows():
+        my_zone = {}
+        my_zone["Address"] = zone["Address"]
+        zone_maps = gmaps.geocode(zone["Address"])
+        zone_maps = zone_maps[0]
+        zone_geometry = zone_maps['geometry']
+        zone_loc = zone_geometry['location']
+        my_zone["Radius"] = zone["Radius"]
+        my_zone["Latitude"] = zone_loc['lat']
+        my_zone['Longitude'] = zone_loc['lng']
+        privacy_zones.append(my_zone)
+
+    return privacy_zones
 
 
 #Takes a list of coords and draws them on mapbox using the associated token
@@ -105,9 +165,34 @@ def draw_map(coords, token_path="mapbox_token.txt"):
     coords = [list(t) for t in coords]
     df = pd.DataFrame(coords, columns = ['latitude', 'longitude'])
     px.set_mapbox_access_token(open(token_path).read())
-    fig = px.scatter_mapbox(data_frame=df, lat='latitude', lon='longitude', size_max=15, zoom=10)
+    fig = px.scatter_mapbox(data_frame=df, lat='latitude', lon='longitude', size_max=15, zoom=7)
     fig.update_layout(title= 'Map of Start Locations')
     fig.show()
 
-#starts = get_start_locations('fit_files')
-#draw_map(starts)
+
+#This will be a class to manage the activity file
+#Will handle importing data from a folder
+#Also want to have maps from an activity file
+class Activity_Manager:
+
+    #Loads in and stores the Dataframe from the activity path
+    def __init__(self, activity_path):
+        self.activity_df = pd.read_csv(activity_path)
+
+    #Returns DataFrame containing only activities of a specified type,
+    #setting gps to true gives only activities with an associated FIT file 
+    def get_activities_of_type(self, activity_type, gps=False):
+        activities = [self.activity_df["Activity Type"] in activity_type]
+
+        #Drop non-mapped activities
+        if gps:
+            activities = activities.dropna(subset=['Filename'])
+
+        return activities
+
+    
+privacy_zone = pd.read_csv('caleb_privacy_zones.csv')
+privacy_coords = get_privacy_coords(privacy_zone)
+
+starts = get_start_locations('fit_files', privacy_zones=privacy_coords)
+draw_map(starts)
